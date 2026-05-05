ed10b4a9e55b8525fba32bb7995787c18409d68c
Arunkumar17-M <1705arunkumar@gmail.com>
2026-05-05 18:53:05 +0530

    Pattern22 question

--- src/patterns/PatternProgram.java
+++ src/patterns/PatternProgram.java
@@ -179,8 +179,21 @@ public class PatternProgram {
         }
     }
 
+    static void pattern22(int n){
+        for(int i = 0; i < 2*n-1 ; i++){
+            for(int j = 0; j < 2*n-1; j++){
+                int top = i;
+                int left = j;
+                int down = (2*n-2)-i;
+                int right = (2*n-2)-j;
+                System.out.print(n-Math.min(Math.min(top, down), Math.min(left, right)));
+            }
+            System.out.println();
+        }
+    }
+    
     public static void main(String[] args) {
 
-        pattern11(5);
+        pattern22(4);
     }
 }
